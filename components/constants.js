1257253f3d777cfc0042a63446819e7d675a6d01
Juuso Pesonen <117353696+JuusoPesonen@users.noreply.github.com>
2024-03-31 23:53:13 +0300

    Added dummy data info for testing

---
+++ components/constants.js
@@ -0,0 +1,49 @@
+//Dummy data for categories and meals
+export const categoryData = [
+    {
+        name: 'Starter',
+        image: 'https://www.themealdb.com/images/category/starter.png'
+    },
+    {
+        name: 'Beef',
+        image: 'https://www.themealdb.com/images/category/beef.png'
+    },
+    {
+        name: 'Dessert',
+        image: 'https://www.themealdb.com/images/category/dessert.png'
+    }
+]
+
+export const mealData = [
+    {
+        id: 1,
+        name: 'Shakshuka',
+        image: 'https://www.themealdb.com/images/media/meals/g373701551450225.jpg',
+        description: 'Delicious Italian dish...'
+        
+    },
+    {
+        id: 2,
+        name: 'Beef Banh Bowls with Sriracha Mayo',
+        image: 'https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg',
+        description: 'Delicious beef dish...'
+    },
+    {
+        id: 3,
+        name: 'Chickpea Fajitas',
+        image: 'https://www.themealdb.com/images/media/meals/tvtxpq1511464705.jpg',
+        description: 'Delious Mexican dish...'
+    },
+    {
+        id: 4,
+        name: 'Smoky Lentil Chili with Squash',
+        image: 'https://www.themealdb.com/images/media/meals/uwxqwy1483389553.jpg',
+        description: 'Delicious chili dish...'
+    },
+    {
+        id: 5,
+        name: 'Braised Beef Chili',
+        image: 'https://www.themealdb.com/images/media/meals/uuqvwu1504629254.jpg',
+        description: 'Tasty beef chili dish...'
+    }
+]
