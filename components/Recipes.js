bd2ff964f6175971e5d44a927bc93399a1fd6fb3
Juuso Pesonen <117353696+JuusoPesonen@users.noreply.github.com>
2024-03-31 23:53:44 +0300

    Added recipes fetch code

---
+++ components/Recipes.js
@@ -0,0 +1,121 @@
+import { View, Text, StyleSheet, ActivityIndicator, Image, ScrollView, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { mealData } from "./constants";
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import Loading from "./loading";
+
+
+const Recipes = ({ categories, meals }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate data fetching (replace with your actual logic)
+        setTimeout(() => {
+
+            setIsLoading(false);
+        }, 1000);
+    }, []);
+
+    return (
+        <View style={styles.recipesContainer}>
+            <Text style={styles.recipesText}>Recipes</Text>
+            {isLoading ? (
+                <View style={styles.activityIndicatorWrapper}>
+                    {categories.length === 0 || meals.length === 0 ? (
+                        <Loading />
+                    ) : (
+                        <ActivityIndicator color={'black'} size={'large'} />
+                    )}
+                </View>
+            ) : (
+                <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                    <View style={styles.cardsContainer}>
+                        {meals.map((item, index) => (
+                            <RecipeCard key={item.idMeal} item={item} index={index} />
+                        ))}
+                    </View>
+                </ScrollView>
+            )}
+        </View>
+    );
+};
+
+// Recipe card
+const RecipeCard = ({ item, index }) => {
+
+    // Check if index is even
+    let isEven = index % 2 === 0;
+
+    return (
+        <Animated.View entering={FadeInDown.delay(index * 100).duration(600).springify().damping(12)} style={styles.recipeCard}>
+            <Pressable style={{ width: '100%', paddingLeft: isEven ? 0 : 8, paddingRight: isEven ? 8 : 0 }}
+            >
+                <Image source={{ uri: item.strMealThumb }} style={styles.recipeImage} />
+                <Text style={styles.recipeName}>
+                    {item.strMeal.length > 20 ? item.strMeal.slice(0, 20) + '...' : item.strMeal}
+                </Text>
+            </Pressable>
+        </Animated.View>
+    )
+};
+
+const styles = StyleSheet.create({
+    recipesContainer: {
+        marginTop: 10,
+        marginHorizontal: 4,
+        marginVertical: 3,
+    },
+
+    recipesText: {
+        fontSize: 18,
+        fontWeight: "semibold",
+    },
+
+    activityIndicatorWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    scrollViewContent: {
+        padding: 10,
+        paddingBottom: 20,
+    },
+
+    recipeCard: {
+        backgroundColor: '#ffd78e',
+        borderRadius: 20,
+        padding: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        marginVertical: 5,
+        width: '48%',
+    },
+
+    cardsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+
+    recipeImage: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
+        borderRadius: 10,
+    },
+
+    recipeName: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+
+    recipeDescription: {
+        fontSize: 12,
+    },
+});
+
+export default Recipes;
