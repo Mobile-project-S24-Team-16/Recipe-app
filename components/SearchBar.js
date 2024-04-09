3072927527b0d3ac7acbd8589910b57a65d05209
Juuso Pesonen <117353696+JuusoPesonen@users.noreply.github.com>
2024-04-09 19:54:58 +0300

    Fixed rendering on searchbar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 
-const SearchBar = ({ handleSubmit, query, isLoading, setQuery }) => {
+const SearchBar = ({ handleSubmit, query, isLoading, setQuery, noResults, clearSearch }) => {
   return (
     <View style={styles.searchBar}>
       <TextInput
         value={query}
         style={styles.inputSearch}
-        placeholder="Search for recipes..."
+        placeholder="Search for a recipe by name or by category"
         onChangeText={setQuery}
         disabled={isLoading}
       />
       <Button title="Search" onPress={handleSubmit} disabled={isLoading} />
+      {clearSearch && <Button title="Clear search" onPress={() => setQuery('')} disabled={isLoading} />}
+      {noResults && <Text style={styles.noResultsText}>Recipe not found!</Text>}
     </View>
   );
 };
@@ -24,7 +26,7 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderWidth: 1,
     paddingHorizontal: 10, 
-    borderRadius: 8, 
+    borderRadius: 10, 
   },
   inputSearch: {
     flex: 1, 
@@ -32,6 +34,11 @@ const styles = StyleSheet.create({
     padding: 10, 
     borderRadius: 5, 
     fontSize: 16, 
+  },
+  noResultsText: {
+    marginTop: 5,
+    color: 'red',
+    textAlign: 'center',
   },
 });
 
