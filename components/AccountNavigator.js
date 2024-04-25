bf7e3a02a363197994b46d124a0d87f6f17d04c1
n2posa00 <n2posa00@students.oamk.fi>
2024-04-25 20:44:39 +0300

    Added icons for top navigation

--- components/AccountNavigator.js
+++ components/AccountNavigator.js
@@ -2,15 +2,41 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import Favorites from '../screens/Favorites';
 import AddRecipe from '../screens/AddRecipe';
 import CreatedRecipes from '../screens/CreatedRecipes';
+import { HeartIcon } from 'react-native-heroicons/solid';
+import { PlusIcon } from 'react-native-heroicons/solid';
+import { ClipboardIcon } from 'react-native-heroicons/solid';
 
 const Tab = createMaterialTopTabNavigator();
 
 const AccountNavigator = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Favorites" component={Favorites} />
-            <Tab.Screen name="AddRecipe" component={AddRecipe} />
-            <Tab.Screen name="CreatedRecipes" component={CreatedRecipes} />
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+                if (route.name === 'Favorites') {
+                    return <HeartIcon size={size} color={color} />;
+                } else if (route.name === 'Add Recipe') {
+                    return <PlusIcon size={size} color={color} />;
+                } else if (route.name === 'My Recipes') {
+                    return <ClipboardIcon size={size} color={color} />;
+                }
+            },
+            tabBarActiveTintColor: '#007AFF',
+            tabBarInactiveTintColor: 'gray',
+            tabBarLabelStyle: { fontSize: 10 },
+        })}
+        >
+            <Tab.Screen 
+            name="Favorites"
+             component={Favorites}
+              />
+            <Tab.Screen 
+            name="Add Recipe" 
+            component={AddRecipe}
+             />
+            <Tab.Screen 
+            name="My Recipes" 
+            component={CreatedRecipes} />
         </Tab.Navigator>
     );
 }
